0b88991855beba10fc4221161864f2b198c831c4
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2019-01-25 17:16:29 +0100

    minor change

--- PALIN/palin.hs
+++ PALIN/palin.hs
@@ -1,10 +1,11 @@
 import Control.Arrow ((>>>), first)
 
+import Data.Word (Word8)
+
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as C8
 
-import Data.Word (Word8)
 
 add :: Word8 -> (Word8, Bool)
 add w 
